e3ce0e0d24b82b21dfbb821363804e1499e3fd06
Oscar Nilsson <oscarandersnilsson@gmail.com>
2022-05-26 13:50:30 +0200

    flask site

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+#from dotenv import load_dotenv
+from flask import Flask
+
+
+def create_app():
+    _app = Flask(__name__)
+    _app.config['SECRET_KEY'] = 'supersecretkey'
+    _app.config['UPLOAD_FOLDER'] = 'static/files'
+
+    from app.blueprints.open import bp_open
+    _app.register_blueprint(bp_open)
+
+    return _app
+
+
+if __name__ == '__main__':
+    #load_dotenv()
+    app = create_app()
+    app.run()
---
+++ app/blueprints/open/__init__.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, send_file
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
+
+bp_open = Blueprint('bp_open', __name__)
+
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+
+@bp_open.route('/', methods=['GET', "POST"])
+@bp_open.route('/home', methods=['GET', "POST"])
+def home(path='C:/Teknikhögskolan/super_resolution/app/static/files'):
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), path, secure_filename(
+            'original_image.jpg'))) # Then save the file
+    return render_template('index.html', form=form)
+
+
+@bp_open.route('/index')
+def show_images():
+    original_image = os.path.join('static/files', 'original_image.jpg')
+    enhanced_image = os.path.join('static/files', 'enhanced_image.jpg')
+    return render_template("images.html", original_image=original_image, enhanced_image=enhanced_image)
+
+
+@bp_open.route('/download')
+def download_file(file='static/files/enhanced_image.jpg'):
+    return send_file(file, as_attachment=True)
